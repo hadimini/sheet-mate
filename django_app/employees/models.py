b9023b700d031c8231d1f93902a9db36cc37b08c
Hadi Mini <hadimini@gmail.com>
2025-10-24 11:27:08 +0300

    update email field

--- django_app/employees/models.py
+++ django_app/employees/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     telegram_id = models.CharField(max_length=50, unique=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
