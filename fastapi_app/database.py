4c33a5282d4ce4d2597f1bef9fb2ccf827b6ef31
Hadi Mini <hadimini@gmail.com>
2025-10-24 12:03:43 +0300

    Add Database Connection Pooling

--- fastapi_app/database.py
+++ fastapi_app/database.py
@@ -1,12 +1,19 @@
 import os
 from typing import AsyncGenerator
 
+from sqlalchemy import QueuePool
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 ASYNC_DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
 
-engine = create_async_engine(ASYNC_DATABASE_URL)
+engine = create_async_engine(
+    ASYNC_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=10,
+    poolclass=QueuePool,
+    max_overflow=20,
+)
 AsyncSessionLocal = async_sessionmaker(eengine=engine, expire_on_commit=False, class_=AsyncSession)
 
 
