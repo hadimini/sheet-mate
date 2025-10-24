142add3d55a48f723cb6dca780a47cf7513d86c8
Hadi Mini <hadimini@gmail.com>
2025-10-24 12:03:24 +0300

    Add django-redis

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -71,3 +71,13 @@ TEMPLATES = [
         },
     },
 ]
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
